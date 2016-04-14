a7cd1d3779fc8e6705a27381e7f7470fb6d7222c
faizankhananics <faizan.khanani@centricsource.com>
2016-04-14 22:10:17 +0500

    button with Layout

--- iOSTrainingWithSwift/ViewController.swift
+++ iOSTrainingWithSwift/ViewController.swift
@@ -13,13 +13,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let abc = "Print"
+        let abc : String = "Print"
         print(abc)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func btnTopLeft(sender: AnyObject) {
+
+    print("You did press Top Left Button")
+    }
+
+
+    @IBAction func btnBottomLeft(sender: AnyObject) {
+
+        print("You did press Bottom Left Button")
+
+    }
+
+
+    @IBAction func btnTopRight(sender: AnyObject) {
+
+            print("You did press Top Right Button")
+    }
+
+    @IBAction func btnBottomRight(sender: AnyObject) {
+
+             print("You did press Bottom Right Button")
     }
 
 
