25048b70469aa0f3f6c0af8a3e7398eb36bee1d3
faizankhananics <faizan.khanani@centricsource.com>
2016-04-14 21:10:21 +0500

    testing

--- iOSTrainingWithSwift/ViewController.swift
+++ iOSTrainingWithSwift/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let a = "Print"
-        print(a)
+        let ab = "Print"
+        print(ab)
     }
 
     override func didReceiveMemoryWarning() {
